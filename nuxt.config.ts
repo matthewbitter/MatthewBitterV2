4ea3854cbbc60f52e8a02b8e0eda0c5758d99fe1
Matthew Bitter <xiero.starlight@gmail.com>
2026-08-23 19:42:22 -0400

    SiteMap Deploy Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,8 @@ export default defineNuxtConfig({
         "@nuxt/image",
         "@nuxt/scripts",
         "@pinia/nuxt",
-        "@nuxt/icon"
+        "@nuxt/icon",
+        "@nuxtjs/sitemap"
     ],
 
     devtools: {
@@ -14,6 +15,12 @@ export default defineNuxtConfig({
     },
 
     css: ["~/assets/css/main.css"],
+
+    site: {
+        url: "https://matthewbitter.com",
+        name: "Matthew Bitter Portfolio",
+        urls: ["/"]
+    },
 
     colorMode: {
         disableTransition: false
