ed047173dcde0cc8a185a303782419a74329d5a3
Matthew Bitter <xiero.starlight@gmail.com>
2026-08-27 16:38:29 -0400

    Skill Search

--- app/components/Index/Skills.vue
+++ app/components/Index/Skills.vue
@@ -1,26 +1,192 @@
 <template>
     <UPageSection id="Skills" title="Skills">
+        <UInput v-model.trim="SkillSearch" icon="i-lucide-search" placeholder="Search" size="lg" class="w-full" />
         <UPageGrid>
-            <UPageCard title="Nuxt" icon="material-icon-theme:nuxt" description="Full-stack web framework built on top of Vue" to="https://nuxt.com" target="_blank" />
-            <UPageCard title="Vue" icon="material-icon-theme:vue" description="Progressive JavaScript framework for building interactive front-end user interfaces and web applications" to="https://vuejs.org/" target="_blank" />
-            <UPageCard title="TypeScript" icon="material-icon-theme:typescript" description="Typed superset of JavaScript for scalable applications" to="https://www.typescriptlang.org" target="_blank" />
-            <UPageCard title="Vuetify" icon="devicon:vuetify" description="UI component library for Vue using Material Design rules" to="https://vuetifyjs.com/en" target="_blank" />
-            <UPageCard title="Nuxt UI" icon="material-icon-theme:nuxt" description="UI component library for Vue using Tailwind CSS" to="https://ui.nuxt.com" target="_blank" />
-            <Index-Skill-Pinia />
-            <UPageCard title="Firebase" icon="material-icon-theme:firebase" description="Platform for building mobile and web applications" to="https://firebase.google.com" target="_blank" />
-            <UPageCard title="Azure" icon="material-icon-theme:azure" description="Public cloud computing platform" to="https://azure.microsoft.com/en-us" target="_blank" />
-            <UPageCard title="Node.js" icon="material-icon-theme:nodejs" description="JavaScript runtime for building scalable network applications" to="https://nodejs.org/en" target="_blank" />
-            <UPageCard title="Unity" icon="material-icon-theme:unity" description="Cross-platform software engine used to build 2D and 3D video games, real-time simulations, and interactive experiences" to="https://unity.com/" target="_blank" />
-            <UPageCard title="C#" icon="material-icon-theme:csharp" description="A modern, object-oriented programming language" to="https://dotnet.microsoft.com/en-us/languages/csharp" target="_blank" />
-            <UPageCard title="ASP.NET" icon="devicon:dotnetcore" description="Server-side web application framework" to="https://dotnet.microsoft.com/en-us/apps/aspnet" target="_blank" />
-            <UPageCard title="GitHub" icon="mdi:github" description="Code hosting platform for version control and collaboration" to="https://github.com" target="_blank" />
-            <UPageCard title="Firestore" icon="thesvg-color:firestore" description="A fully managed, serverless NoSQL document database to store, sync, and query data" to="https://firebase.google.com/docs/firestore" target="_blank" />
-            <UPageCard title="reCAPTCHA" icon="logos:recaptcha" description="Protects your site from spam and abuse. It uses advanced risk analysis techniques to tell humans and bots apart" to="https://developers.google.com/recaptcha" target="_blank" />
-            <UPageCard title="Resend" icon="thesvg-color:resend-dark" description="A modern email delivery platform and API built specifically for developers" to="https://resend.com/" target="_blank" />
-            <UPageCard title="MSSQL" icon="mdi:database" description="A relational database management system" to="https://www.microsoft.com/en-us/sql-server" target="_blank" />
-            <UPageCard title="Cloudflare" icon="devicon:cloudflare" description="A global cloud platform that provides security, speed, and reliability services for the web" to="https://www.cloudflare.com/" target="_blank" />
-            <UPageCard title="Google Cloud Platform (GCP)" icon="material-icon-theme:gcp" description="A suite of cloud computing services that lets you build, run, and scale applications" to="https://cloud.google.com/" target="_blank" />
+            <template v-for="(skill, index) of FilteredSkills" :key="index">
+                <component :is="skill.Component" v-if="skill.Component" />
+                <UPageCard v-else :title="skill.Name" :icon="(typeof skill.Icon === 'function') ? skill.Icon() : skill.Icon" :description="skill.Description" :to="skill.Url" target="_blank" />
+            </template>
         </UPageGrid>
     </UPageSection>
 </template>
 
+<script setup lang="ts">
+
+//---------------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------------
+import type { ConcreteComponent } from "vue";
+
+
+//---------------------------------------------------------------------------
+// Properties
+//---------------------------------------------------------------------------
+type SkillType = { Name?: string, Description?: string, Url?: string, Icon?: string | (() => string), Score: number, Component?: string | ConcreteComponent };
+const IndexSkillPinia = resolveComponent("Index-Skill-Pinia");
+const SkillSearch = ref("");
+const FilteredSkills = computed(() => Skills.filter(skill => skill.Name?.toLowerCase().includes(SkillSearch.value.toLowerCase()) || skill.Description?.toLowerCase().includes(SkillSearch.value.toLowerCase())));
+const Skills = reactive<SkillType[]>([]);
+
+
+const skills: SkillType[] = [
+    {
+        Name: "Nuxt",
+        Description: "Full-stack web framework built on top of Vue",
+        Url: "https://nuxt.com",
+        Icon: "material-icon-theme:nuxt",
+        Score: 100
+    },
+    {
+        Name: "Vue",
+        Description: "Progressive JavaScript framework for building interactive front-end user interfaces and web applications",
+        Url: "https://vuejs.org/",
+        Icon: "material-icon-theme:vue",
+        Score: 100
+    },
+    {
+        Name: "TypeScript",
+        Description: "Typed superset of JavaScript for scalable applications",
+        Url: "https://www.typescriptlang.org",
+        Icon: "material-icon-theme:typescript",
+        Score: 100
+    },
+    {
+        Name: "Vuetify",
+        Description: "UI component library for Vue using Material Design rules",
+        Url: "https://vuetifyjs.com/en",
+        Icon: "devicon:vuetify",
+        Score: 0
+    },
+    {
+        Name: "Nuxt UI",
+        Description: "UI component library for Vue using Tailwind CSS",
+        Url: "https://ui.nuxt.com",
+        Icon: "material-icon-theme:nuxt",
+        Score: 0
+    },
+    markRaw({
+        Name: "Pinia",
+        Description: "The official state management library for Vue.js",
+        Component: IndexSkillPinia,
+        Score: 0
+    }),
+    {
+        Name: "Firebase",
+        Description: "Platform for building mobile and web applications",
+        Url: "https://firebase.google.com",
+        Icon: "material-icon-theme:firebase",
+        Score: 0
+    },
+    {
+        Name: "Azure",
+        Description: "Public cloud computing platform",
+        Url: "https://azure.microsoft.com/en-us",
+        Icon: "material-icon-theme:azure",
+        Score: 0
+    },
+    {
+        Name: "Node.js",
+        Description: "JavaScript runtime for building scalable network applications",
+        Url: "https://nodejs.org/en",
+        Icon: "material-icon-theme:nodejs",
+        Score: 0
+    },
+    {
+        Name: "Unity",
+        Description: "Cross-platform software engine used to build 2D and 3D video games, real-time simulations, and interactive experiences",
+        Url: "https://unity.com/",
+        Icon: "material-icon-theme:unity",
+        Score: 0
+    },
+    {
+        Name: "C#",
+        Description: "A modern, object-oriented programming language",
+        Url: "https://dotnet.microsoft.com/en-us/languages/csharp",
+        Icon: "material-icon-theme:csharp",
+        Score: 0
+    },
+    {
+        Name: "ASP.NET",
+        Description: "Server-side web application framework",
+        Url: "https://dotnet.microsoft.com/en-us/apps/aspnet",
+        Icon: "devicon:dotnetcore",
+        Score: 0
+    },
+    {
+        Name: "GitHub",
+        Description: "Code hosting platform for version control and collaboration",
+        Url: "https://github.com",
+        Icon: "mdi:github",
+        Score: 0
+    },
+    {
+        Name: "Firestore",
+        Description: "A fully managed, serverless NoSQL document database to store, sync, and query data",
+        Url: "https://firebase.google.com/docs/firestore",
+        Icon: "thesvg-color:firestore",
+        Score: 0
+    },
+    {
+        Name: "reCAPTCHA",
+        Description: "Protects your site from spam and abuse. It uses advanced risk analysis techniques to tell humans and bots apart",
+        Url: "https://developers.google.com/recaptcha",
+        Icon: "logos:recaptcha",
+        Score: 0
+    },
+    {
+        Name: "Resend",
+        Description: "A modern email delivery platform and API built specifically for developers",
+        Url: "https://resend.com/",
+        Icon: "thesvg-color:resend-dark",
+        Score: 0
+    },
+    {
+        Name: "MSSQL",
+        Description: "A relational database management system",
+        Url: "https://www.microsoft.com/en-us/sql-server",
+        Icon: "mdi:database",
+        Score: 0
+    },
+    {
+        Name: "Cloudflare",
+        Description: "A global cloud platform that provides security, speed, and reliability services for the web",
+        Url: "https://www.cloudflare.com/",
+        Icon: "devicon:cloudflare",
+        Score: 0
+    },
+    {
+        Name: "Google Cloud Platform (GCP)",
+        Description: "A suite of cloud computing services that lets you build, run, and scale applications",
+        Url: "https://cloud.google.com/",
+        Icon: "material-icon-theme:gcp",
+        Score: 0
+    }
+];
+
+skills.sort((skillA, skillB) => skillB.Score - skillA.Score);
+
+Skills.push(...skills);
+
+
+//---------------------------------------------------------------------------
+/**
+ * Runs after component renders in the DOM
+ */
+//---------------------------------------------------------------------------
+onMounted(() =>
+{
+
+    const resendSkill = Skills.find(skill => skill.Name === "Resend");
+
+    if (!resendSkill)
+    {
+
+        return;
+
+    }
+
+    resendSkill.Icon = () => `thesvg-color:resend-${useColorMode().value}`;
+
+});
+
+</script>
