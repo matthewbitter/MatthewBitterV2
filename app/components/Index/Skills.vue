dfd0e2fe0433b90d3b94dcaca8fa540e852be437
Matthew Bitter <xiero.starlight@gmail.com>
2026-08-27 12:19:13 -0400

    Add Additional Skills

--- app/components/Index/Skills.vue
+++ app/components/Index/Skills.vue
@@ -14,6 +14,13 @@
             <UPageCard title="C#" icon="material-icon-theme:csharp" description="A modern, object-oriented programming language" to="https://dotnet.microsoft.com/en-us/languages/csharp" target="_blank" />
             <UPageCard title="ASP.NET" icon="devicon:dotnetcore" description="Server-side web application framework" to="https://dotnet.microsoft.com/en-us/apps/aspnet" target="_blank" />
             <UPageCard title="GitHub" icon="mdi:github" description="Code hosting platform for version control and collaboration" to="https://github.com" target="_blank" />
+            <UPageCard title="Firestore" icon="thesvg-color:firestore" description="A fully managed, serverless NoSQL document database to store, sync, and query data" to="https://firebase.google.com/docs/firestore" target="_blank" />
+            <UPageCard title="reCAPTCHA" icon="logos:recaptcha" description="Protects your site from spam and abuse. It uses advanced risk analysis techniques to tell humans and bots apart" to="https://developers.google.com/recaptcha" target="_blank" />
+            <UPageCard title="Resend" icon="thesvg-color:resend-dark" description="A modern email delivery platform and API built specifically for developers" to="https://resend.com/" target="_blank" />
+            <UPageCard title="MSSQL" icon="mdi:database" description="A relational database management system" to="https://www.microsoft.com/en-us/sql-server" target="_blank" />
+            <UPageCard title="Cloudflare" icon="devicon:cloudflare" description="A global cloud platform that provides security, speed, and reliability services for the web" to="https://www.cloudflare.com/" target="_blank" />
+            <UPageCard title="Google Cloud Platform (GCP)" icon="material-icon-theme:gcp" description="A suite of cloud computing services that lets you build, run, and scale applications" to="https://cloud.google.com/" target="_blank" />
         </UPageGrid>
     </UPageSection>
 </template>
+
