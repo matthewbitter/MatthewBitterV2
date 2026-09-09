4c06c53a50897ba8bd13e666d52610138a1bde0d
Matthew Bitter <xiero.starlight@gmail.com>
2026-09-09 06:48:48 -0400

    Update SEO Meta

--- app/app.vue
+++ app/app.vue
@@ -32,14 +32,17 @@ useHead({
 });
 
 const title = "Matthew Bitter";
-const description = "Senior Full-Stack Developer and Unity Game Developer based in Cincinnati, Ohio.";
+const description = "Matthew Bitter is a Senior Full-Stack Developer with over 20+ years of experience, specializing in building high-performance, scalable, responsive applications using Vue, Nuxt, and TypeScript.";
 
 useSeoMeta({
     title,
     description,
     ogTitle: title,
     ogDescription: description,
-    twitterCard: "summary_large_image"
+    twitterCard: "summary_large_image",
+    ogImage: "https://matthewbitter.com/Images/MatthewBitter.png",
+    author: "Matthew Bitter",
+    ogType: "website"
 });
 
 </script>
